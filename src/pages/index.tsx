0341b7c9e856a919c4cd46c533165dfe30b9e423
Renan Veronez Drechsler <renanvz@outlook.com>
2021-12-01 18:54:04 -0300

    Second section home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,20 +27,61 @@ export default function Home() {
       </div>
       <div className={styles.containerHistory}>
         <main className={styles.contentContainerB}>
-          <div className={styles.left}>
-            <h1>Histórias e Experiências</h1>
-            <p>
-              <span>...for brazilians 🇧🇷</span>
-            </p>
-          </div>
-          <div className={styles.right}>
-            <h1>Histórias e Experiências</h1>
-            <p>
-              <span>...for brazilians 🇧🇷</span>
-            </p>
+          <div className={styles.contentText}>
+            <p><b>About Cork</b> é um blog que tem o objetivo de integrar a comunidade brasileira na cidade de Cork, na Irlanda.</p>
+            <p>A ideia é trazer conteúdo de forma simples, rápida e direta sobre dúvidas, dicas, histórias e recomendações!</p>
+            <p>Se você deseja contribuir indicando perguntas ou complementar respostas, divulgar sua história ou trabalho, teremos o maior prazer em disponibilizar o espaço.</p>
+            <p>After all, this is Cork for brazilians!!</p>
+
           </div>
 
+          <div className={styles.containerTopics}>
 
+            <div className={styles.topics}>
+              <div className={styles.left}>
+                <div className={styles.contentTopic}>
+                  <h1>Histórias e experiências</h1>
+                  <p>
+                    <span>Conheça histórias de quem já foi!</span>
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.topics}>
+                <div className={styles.right}>
+                  <div className={styles.contentTopic}>
+                    <h1>Dúvidas frequentes</h1>
+                    <p>
+                      <span>Encontre a resposta para aquela dúvida que cedo ou tarde vai acontecer!</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.topics}>
+                <div className={styles.left}>
+                  <div className={styles.contentTopic}>
+                    <h1>Dicas</h1>
+                    <p>
+                      <span>Conheça histórias de quem já foi!</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.topics}>
+                <div className={styles.right}>
+                  <div className={styles.contentTopic}>
+                    <h1>Galeria</h1>
+                    <p>
+                      <span>Confira fotos de lugares turísticos e eventos!</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
         </main>
       </div>
     </>
