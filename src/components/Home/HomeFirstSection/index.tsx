746341557fc1d07b4f91dedccb92c0a1b9c75120
Renan Veronez Drechsler <renanvz@outlook.com>
2022-02-09 15:55:55 -0300

    Particles

--- src/components/Home/HomeFirstSection/index.tsx
+++ src/components/Home/HomeFirstSection/index.tsx
@@ -1,14 +1,58 @@
 import styles from "./styles.module.scss";
-import MyParticles from '../Particles'
+// import MyParticles from '../Particles'
+import Particles from "react-particles-js";
 
 export function HomeFirstSection() {
   return (
     <>
       <div data-aos="fade-up" >
-        <MyParticles></MyParticles>
+
         <div className={styles.containerHome}>
+          <Particles
+            className={styles.particles}
+            params={{
+              "particles": {
+                "number": {
+                  "value": 150,
+                  "density": {
+                    "enable": true,
+                    "value_area": 1500
+                  }
+                },
+                "line_linked": {
+                  "enable": false,
+                },
+                "move": {
+                  // "direction": "down",
+                  "speed": 0.1
+                },
+                "size": {
+                  "value": 2
+                },
+                "opacity": {
+                  "anim": {
+                    "enable": true,
+                    "speed": 1.5,
+                    "opacity_min": 0.05
+                  }
+                }
+              },
+              "interactivity": {
+                "events": {
+                  "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                  }
+                },
+                "modes": {
+                  "push": {
+                    "particles_nb": 1
+                  }
+                }
+              },
+              "retina_detect": true
+            }} />
           <main className={styles.contentContainer}>
-            <MyParticles></MyParticles>
             <div className={styles.left}>
               <span>👏🏼 Hey, welcome to</span>
               <h1>About <span>Cork</span></h1>
@@ -16,11 +60,13 @@ export function HomeFirstSection() {
                 <span>...for brazilians 🇧🇷</span>
               </p>
             </div>
-            <MyParticles></MyParticles>
+
             <div className={styles.right}>
               <img src="/images/irish.png" alt="ireland" />
             </div>
           </main>
+
+
         </div>
       </div>
     </>
--- src/components/Home/Particles/index.tsx
+++ src/components/Home/Particles/index.tsx
@@ -1,52 +1,54 @@
 import Particles from "react-particles-js";
 import styles from './styles.module.scss';
 
-export default function MyParticles() {
+export default function MyParticles({ children }) {
   return (
-    <div className={styles.particles}>
-      <Particles
-        params={{
-          "particles": {
-            "number": {
-              "value": 150,
-              "density": {
-                "enable": true,
-                "value_area": 1500
-              }
-            },
-            "line_linked": {
-              "enable": false,
-            },
-            "move": {
-              // "direction": "down",
-              "speed": 0.1
-            },
-            "size": {
-              "value": 2
-            },
-            "opacity": {
-              "anim": {
-                "enable": true,
-                "speed": 1.5,
-                "opacity_min": 0.05
-              }
+    <Particles
+      className={styles.particles}
+      params={{
+        "particles": {
+          "number": {
+            "value": 150,
+            "density": {
+              "enable": true,
+              "value_area": 1500
             }
           },
-          "interactivity": {
-            "events": {
-              "onclick": {
-                "enable": true,
-                "mode": "push"
-              }
-            },
-            "modes": {
-              "push": {
-                "particles_nb": 1
-              }
+          "line_linked": {
+            "enable": false,
+          },
+          "move": {
+            // "direction": "down",
+            "speed": 0.1
+          },
+          "size": {
+            "value": 2
+          },
+          "opacity": {
+            "anim": {
+              "enable": true,
+              "speed": 1.5,
+              "opacity_min": 0.05
+            }
+          }
+        },
+        "interactivity": {
+          "events": {
+            "onclick": {
+              "enable": true,
+              "mode": "push"
             }
           },
-          "retina_detect": true
-        }} />
-    </div>
+          "modes": {
+            "push": {
+              "particles_nb": 1
+            }
+          }
+        },
+        "retina_detect": true
+      }}>
+      {children}
+
+    </Particles>
   );
 }
